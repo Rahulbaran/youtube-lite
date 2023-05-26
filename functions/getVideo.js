c57916c154989a04988f9fb94e00f49a1af5d2ae
Rahulkumar <rahulkumar109422@gmail.com>
2023-05-26 15:17:02 +0530

    feat: create netlify configuration and netlify fuctions directory

---
+++ functions/getVideo.js
@@ -0,0 +1,14 @@
+const handler = async (event) => {
+    try {
+      const subject = event.queryStringParameters.name || 'World'
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: `Hello ${subject}` })
+      }
+    } catch (error) {
+      return { statusCode: 500, body: error.toString() }
+    }
+  };
+  
+  export default handler;
+  
