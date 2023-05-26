8c4557ce852c8c67a3ddacae03242bf5b25c860a
Rahulkumar <rahulkumar109422@gmail.com>
2023-05-26 18:10:58 +0530

    feat: create functionality to fetch videos
    - used "axios" to fetch videos from "Rapid API"
    - returned the videos

--- functions/getAllVideos.js
+++ functions/getAllVideos.js
@@ -1,13 +1,36 @@
-const handler = async (event) => {
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
+
+export const handler = async () => {
+  const YT_URL = process.env.YT_URL;
+  const RAPID_API_KEY = process.env.RAPID_API_KEY;
+  console.log(YT_URL, RAPID_API_KEY);
+
+  const options = {
+    method: "GET",
+    url: YT_URL,
+    params: {
+      relatedToVideoId: "7ghhRHRP6t4",
+      part: "id,snippet",
+      type: "video",
+      maxResults: "50"
+    },
+    headers: {
+      "X-RapidAPI-Key": RAPID_API_KEY,
+      "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com"
+    }
+  };
+
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const response = await axios.request(options);
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
-    }
+      body: JSON.stringify({ videos: response.data })
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
 };
-
-export default handler;
