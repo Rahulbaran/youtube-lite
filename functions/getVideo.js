ec4072301e5b5b54ab34a4b6119f901875a3e88b
Rahulkumar <rahulkumar109422@gmail.com>
2023-05-28 18:28:59 +0530

    feat: fetch data of a video using netlify function

--- functions/getVideo.js
+++ functions/getVideo.js
@@ -1,14 +1,34 @@
-const handler = async (event) => {
-    try {
-      const subject = event.queryStringParameters.name || 'World'
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: `Hello ${subject}` })
-      }
-    } catch (error) {
-      return { statusCode: 500, body: error.toString() }
+import dotenv from "dotenv";
+import axios from "axios";
+dotenv.config();
+
+const VIDEO_URL = process.env.VIDEO_URL;
+const RAPID_API_KEY = process.env.RAPID_API_KEY;
+
+export const handler = async event => {
+  const vId = event.queryStringParameters.videoId;
+
+  const options = {
+    method: "GET",
+    url: VIDEO_URL,
+    params: {
+      part: "contentDetails,snippet,statistics",
+      id: vId
+    },
+    headers: {
+      "X-RapidAPI-Key": RAPID_API_KEY,
+      "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com"
     }
   };
-  
-  export default handler;
-  
+
+  try {
+    const res = await axios.request(options);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data: res.data })
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
