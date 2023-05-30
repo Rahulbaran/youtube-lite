393f00cbf4fa9bdb01e633ddf9510477abc2904c
Rahulkumar <rahulkumar109422@gmail.com>
2023-05-30 08:05:30 +0530

    feat: use "html-react-parser" to parse raw html string

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,3 +1,4 @@
+import parser from "html-react-parser";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
@@ -11,7 +12,7 @@ export default function CommentCard({ comment }) {
         <small>{commentData.snippet.authorDisplayName}</small>
         <small>{dayjs(commentData.snippet.publishedAt).fromNow()}</small>
       </div>
-      <p class="comment-label">{commentData.snippet.textDisplay}</p>
+      <p class="comment-label">{parser(commentData.snippet.textDisplay)}</p>
     </div>
   );
 }
