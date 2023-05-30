df9175d1a402783d4fa1effd656ba396bafad719
Rahulkumar <rahulkumar109422@gmail.com>
2023-05-30 15:31:24 +0530

    feat: fetch data of a channel using netlify function

---
+++ functions/getChannel.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
+
+const RAPID_API_KEY = process.env.RAPID_API_KEY;
+
+export const handler = async event => {
+  const channelId = event.queryStringParameters.channelId;
+
+  const options = {
+    method: "GET",
+    url: "https://youtube-v31.p.rapidapi.com/channels",
+    params: {
+      part: "snippet,statistics",
+      id: channelId
+    },
+    headers: {
+      "X-RapidAPI-Key": RAPID_API_KEY,
+      "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com"
+    }
+  };
+
+  try {
+    const res = await axios.request(options);
+
+    return { statusCode: 200, body: JSON.stringify(res.data) };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "video id is not valid" })
+    };
+  }
+};
